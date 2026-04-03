54669ad2e8dd215352e8197fe44853d4e5bf998f
Walassi Silva <walassi.b@gmail.com>
2026-04-02 22:06:08 -0300

    feat: add DeleteUserUseCase

---
+++ src/use-cases/delete-user.js
@@ -0,0 +1,9 @@
+import { PostgresDeleteUserRepository } from "../repositories/postgres/index.js";
+
+export class DeleteUserUseCase {
+  async execute(userId) {
+    const deleteUserRepository = new PostgresDeleteUserRepository();
+    const deletedUser = deleteUserRepository.execute(userId);
+    return deletedUser;
+  }
+}
