b8d1d2b5d660abd12ef267b46e659169d32e9b72
Walassi Silva <walassi.b@gmail.com>
2026-04-05 09:42:22 -0300

    feat: add createTransactionUseController

---
+++ src/controllers/transaction/create-transaction.js
@@ -0,0 +1,65 @@
+import validator from "validator";
+import {
+  badRequest,
+  created,
+  invalidIdResponse,
+  serverError,
+  validateId,
+} from "../helpers/index.js";
+
+export class CreateTransactionController {
+  constructor(createTransactionUseCase) {
+    this.createTransactionUseCase = createTransactionUseCase;
+  }
+  async execute(httpRequest) {
+    try {
+      const params = httpRequest.body;
+      const requiredFields = [
+        "id",
+        "user_id",
+        "name",
+        "amount",
+        "date",
+        "type",
+      ];
+      for (const field of requiredFields) {
+        if (!params[field] || params[field].trim().length === 0) {
+          return badRequest({ message: `Missing param: ${field}` });
+        }
+      }
+
+      const isUserIdValid = validateId(params.userId);
+      if (!isUserIdValid) {
+        return invalidIdResponse();
+      }
+
+      if (params.amount <= 0) {
+        return badRequest({
+          message: "The amount must be greatter than 0.",
+        });
+      }
+      const isAmountValid = validator.isCurrency({
+        digits_after_decimal: [2],
+        allow_negatives: false,
+        decimal_separator: ".",
+      });
+      if (!isAmountValid) {
+        return badRequest({ message: "The amount must be a valid currency." });
+      }
+
+      const type = params.type.trim().toUpperCase();
+      const isTypeValid = ["INCOME", "EXPENSE", "INVESTMENT"].includes(type);
+      if (!isTypeValid) {
+        badRequest({ message: "The must be INCOME, EXPENSE or INVESTMENT." });
+      }
+      const transaction = await this.createTransactionUseCase.execute({
+        ...params,
+        type,
+      });
+      return created(transaction);
+    } catch (error) {
+      console.error(error);
+      return serverError();
+    }
+  }
+}
