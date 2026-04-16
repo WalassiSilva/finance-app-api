cee4fe6ec50d93b18e4321788f9c2e89e7cf741a
Walassi Silva <walassi.b@gmail.com>
2026-04-15 22:51:05 -0300

    test: add error tests to createUserController

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -1,3 +1,4 @@
+import { EmailAlreadyInUseError } from "../../errors/user";
 import { CreateUserController } from "./create-user";
 import { faker } from "@faker-js/faker";
 describe("Create user Controller", () => {
@@ -137,5 +138,47 @@ describe("Create user Controller", () => {
     //assert
 
     expect(executeSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  it("Should return 500 if CreateUserUseCase throws an Error", async () => {
+    //arrange
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+    const httpRequest = {
+      body: {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      },
+    };
+    //act
+    jest.spyOn(createUserUseCase, "execute").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const result = await createUserController.execute(httpRequest);
+    //assert
+    expect(result.statusCode).toBe(500);
+  });
+
+  it("Should return 400 if CreateUserUseCase throws EmailAlreadyInUseError", async () => {
+    //arrange
+    const createUserUseCase = new CreateUserUseCaseStub();
+    const createUserController = new CreateUserController(createUserUseCase);
+    const httpRequest = {
+      body: {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      },
+    };
+    //act
+    jest.spyOn(createUserUseCase, "execute").mockImplementationOnce(() => {
+      throw new EmailAlreadyInUseError(httpRequest.body.email);
+    });
+    const result = await createUserController.execute(createUserUseCase);
+    //assert
+    expect(result.statusCode).toBe(400);
   });
 });
